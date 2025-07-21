9d852cf1cb3b4049460351daca4e6740d1243521
Mohana Vignesh <areymohanlive@gmail.com>
2025-07-21 17:43:06 +0530

    init

--- src/ArrayAndString.java
+++ src/ArrayAndString.java
@@ -35,14 +35,57 @@ public class ArrayAndString {
         for (int i = 0; i < nums.length; i++) {
             int ans = target - nums[i];
             if (visitedNumbers.containsKey(ans)) {
-                return new int[] {i, visitedNumbers.get(ans)};
+                return new int[]{i, visitedNumbers.get(ans)};
             }
             visitedNumbers.put(nums[i], i);
         }
-        return new int[] {-1, -1};
+        return new int[]{-1, -1};
     }
 
     public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+        return sb.toString().equals(sb.reverse().toString());
+        /*
+        if (s.length() == 0) {
+            return true;
+        }
+        int l = 0, r = s.length() - 1;
 
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+         */
     }
+
+    public int maxProfit(int[] prices) {
+        int pist = 0;
+        int maxProfit = 0;
+        int lsp = Integer.MAX_VALUE;
+        for (int i : prices) {
+            if (i < lsp)
+                lsp = i;
+            pist = i - lsp;
+            if (pist > maxProfit) {
+                maxProfit = pist;
+            }
+        }
+        return maxProfit;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Main obj = new Main();
         ArrayAndString arr = new ArrayAndString();
-
+        System.out.println(arr.isPalindrome("A man, a plan, a canal: Panama"));
     }
 }
